c07d67c4f5cc45496741fdc73cc6ab07a1c4f251
ahmed samir <ahmed.samir.farid@gmail.com>
2023-05-02 16:53:23 +0300

    V 0.1

--- components/widget/SocialMedia.vue
+++ components/widget/SocialMedia.vue
@@ -1,11 +1,72 @@
 <template>
     <div class="SocialMedia text-center my-5">
       <ul class="pa-0">
-          <li class="Social-icon"><a class="text-color-default" href="" title="Facebook" ><i class="fa-brands fa-facebook-f"></i></a></li>
-          <li class="Social-icon"><a class="text-color-default" href="" title="Twitter"><i class="fa-brands fa-twitter"></i></a></li>
-          <li class="Social-icon"><a class="text-color-default" href="" title="Instagram"><i class="fa-brands fa-instagram"></i></a></li>
-          <li class="Social-icon"><a class="text-color-default" href="" title="linkedin"><i class="fa-brands fa-linkedin"></i></a></li>
-          <li class="Social-icon"><a class="text-color-default" href="" title="Github"><i class="fa-brands fa-github"></i></a></li>
+
+        <v-tooltip
+                v-for="(social, index) in SocialIcon"
+                :key="index"
+                :to="social.to"
+                class="mx-1"
+                router
+                icon
+                exact
+              >
+              <template v-slot:activator="{ on, attrs }">
+        <div class="pa-0">
+
+          <li class="Social-icon" v-bind="attrs" v-on="on"><a class="text-color-default" href="https://www.facebook.com/a7med.samir.93/" target="_blank" title="social.title" >
+            <v-icon   class="white--text" v-text="social.icon"></v-icon>
+          </a></li>
+</div>
+
+      </template>
+      <span> {{social.title}}</span>
+
+          
+              </v-tooltip>
+
+
+
+
+
+
+<!-- 
+        <v-tooltip left  color="#3a37eb">
+      <template v-slot:activator="{ on, attrs }">
+        <div class="pa-0">
+
+          <li class="Social-icon" v-bind="attrs" v-on="on"><a class="text-color-default" href="https://www.facebook.com/a7med.samir.93/" target="_blank" title="social.title" ><i class="social.Icon"></i></a></li>
+</div>
+
+      </template>
+      <span>Facebook</span>
+    </v-tooltip>
+
+
+
+
+    <v-tooltip left  color="#3a37eb">
+      <template v-slot:activator="{ on, attrs }">
+        <div class="pa-0">
+
+
+
+          <li class="Social-icon" v-bind="attrs" v-on="on"><a class="text-color-default" href="" target="_blank" title="Twitter"><i class="fa-brands fa-twitter"></i></a></li>
+</div>
+
+      </template>
+      <span>Twitter</span>
+    </v-tooltip>
+
+
+
+
+
+          <li class="Social-icon"><a class="text-color-default" href="" target="_blank" title="Instagram"><i class="fa-brands fa-instagram"></i></a></li>
+          <li class="Social-icon"><a class="text-color-default" href="https://www.linkedin.com/in/a7med-samir-f/" target="_blank" title="linkedin"><i class="fa-brands fa-linkedin"></i></a></li>
+          <li class="Social-icon"><a class="text-color-default" href="https://github.com/a7medsamirf" target="_blank" title="Github"><i class="fa-brands fa-github"></i></a></li> -->
+
+
       </ul>
     </div>
   </template>
@@ -15,7 +76,10 @@
       name: "SocialMedia",
       data() {
         return {
-          SocialIcon: []
+          SocialIcon: [
+                { Icon: "fa-brands fa-facebook-f", title: 'Facebook', to: '#'},
+
+        ]
         }
       }
     }
